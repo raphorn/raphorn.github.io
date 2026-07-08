18f71366c188cf7aba7e804e319a507414d625cb
raphorn <raashedmusic4@gmail.com>
2026-07-09 01:19:35 +0330

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,1 +1,76 @@
+const songs = [
 
+{
+title:"ریمیکس اول",
+artist:"RAPHORN",
+file:"music/song1.mp3",
+cover:"images/cover1.jpg"
+},
+
+{
+title:"ریمیکس دوم",
+artist:"RAPHORN",
+file:"music/song2.mp3",
+cover:"images/cover2.jpg"
+},
+
+{
+title:"ریمیکس سوم",
+artist:"RAPHORN",
+file:"music/song3.mp3",
+cover:"images/cover3.jpg"
+}
+
+];
+const list = document.getElementById("music-list");
+
+
+songs.forEach(song=>{
+
+
+list.innerHTML += `
+
+<div class="release-card">
+
+
+<div class="release-cover">
+
+<img src="${song.cover}">
+
+</div>
+
+
+<div class="release-content">
+
+<h3>${song.title}</h3>
+
+<p>${song.artist}</p>
+
+
+<button onclick="playSong('${song.file}')">
+
+▶ پخش
+
+</button>
+
+
+</div>
+
+
+</div>
+
+`;
+
+
+});
+
+
+function playSong(file){
+
+let audio = document.getElementById("audio");
+
+audio.src=file;
+
+audio.play();
+
+}
