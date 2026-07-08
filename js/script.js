8258494c54c3d82f2fb516626ae896c3660535ff
raphorn <raashedmusic4@gmail.com>
2026-07-09 01:16:29 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 
+const playlist = [];
+
+const audio = new Audio();
+
+let currentTrack = 0;
+
+const playButton = document.querySelector(".player button");
+const trackName = document.querySelector(".track");
+
+playButton.addEventListener("click",()=>{
+
+if(playlist.length===0){
+
+alert("No track available.");
+
+return;
+
+}
+
+if(audio.paused){
+
+audio.play();
+
+playButton.innerHTML="❚❚";
+
+}else{
+
+audio.pause();
+
+playButton.innerHTML="▶";
+
+}
+
+});
+
+audio.addEventListener("ended",()=>{
+
+playButton.innerHTML="▶";
+
+});
+const menuBtn = document.querySelector(".menu-btn");
+const sidebar = document.getElementById("sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
