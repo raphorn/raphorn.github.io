0a1d8ba4749014fe22ca87e139183c7b5a74e27e
raphorn <raashedmusic4@gmail.com>
2026-07-09 00:23:23 +0330

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,1 +1,28 @@
 
+const playlist = [
+
+{
+    title: "اولین موزیک",
+    artist: "RAPHORN",
+    cover: "images/cover1.jpg",
+    audio: "music/song1.mp3",
+    download: "music/song1.mp3"
+},
+
+{
+    title: "ریمیکس دوم",
+    artist: "RAPHORN",
+    cover: "images/cover2.jpg",
+    audio: "music/song2.mp3",
+    download: "music/song2.mp3"
+},
+
+{
+    title: "ریمیکس سوم",
+    artist: "RAPHORN",
+    cover: "images/cover3.jpg",
+    audio: "music/song3.mp3",
+    download: "music/song3.mp3"
+}
+
+];
