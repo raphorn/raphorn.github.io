7833b068a6b721dd668dea6efab465d5d9d355dc
raphorn <raashedmusic4@gmail.com>
2026-07-09 03:56:59 +0330

    Fix file path for song1.mp3 in music.js

--- js/music.js
+++ js/music.js
@@ -3,7 +3,7 @@ const songs = [
 {
 title: "ریمیکس جديد بنام شوم از راشد",
 artist: "RAASHED",
-file: "music/musi/song1.mp3",
+file: "music/music/song1.mp3",
 cover: "images/file_000000008164722faab306d6308856f4.png"
 },
 
