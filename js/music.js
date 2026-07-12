b0e305f0efd12c2a48e69c734631fadbb1bd4d75
raphorn <raashedmusic4@gmail.com>
2026-07-13 01:50:56 +0330

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,20 +1,66 @@
+const songs = [
+
+{
+title: "ریمیکس جدید بنام شوم از راشد",
+artist: "RAASHED",
+file: "music/music/beat_2.mp3 (Remix).mp3",
+cover: "images/file_000000008164722faab306d6308856f4.png"
+},
+
+{
+title: "ریمیکس دوم",
+artist: "RAPHORN",
+file: "music/song2.mp3",
+cover: "images/cover2.jpg"
+},
+
+{
+title: "ریمیکس سوم",
+artist: "RAPHORN",
+file: "music/song3.mp3",
+cover: "images/cover3.jpg"
+}
+
+];
+
+
+const list = document.getElementById("music-list");
+
+const audio = document.getElementById("audio");
+
+
+const playerTitle = document.getElementById("player-title");
+
+const playerArtist = document.getElementById("player-artist");
+
+const playerCover = document.getElementById("player-cover");
+
+
+let currentSong = 0;
+
+
+
 function showSongs(){
 
 list.innerHTML = "";
 
+
 songs.forEach((song,index)=>{
+
 
 list.innerHTML += `
 
+
 <div class="music-card">
+
 
 <div class="music-cover">
 
 <img src="${song.cover}" alt="${song.title}">
 
-<div class="vinyl"></div>
-
 </div>
+
+
 
 <div class="music-info">
 
@@ -24,9 +70,15 @@ list.innerHTML += `
 
 </div>
 
+
+
 <div class="music-progress">
 
-<span class="current-time">0:00</span>
+
+<span class="current-time">
+0:00
+</span>
+
 
 <input
 type="range"
@@ -37,47 +89,174 @@ class="seek-bar"
 id="seek-${index}"
 >
 
-<span class="duration">0:00</span>
+
+<span class="duration">
+0:00
+</span>
+
 
 </div>
 
+
+
+
 <div class="music-controls">
 
-<button class="play-btn-card"
-onclick="playSong(${index})">
+
+<button onclick="playSong(${index})">
 
 Play
 
 </button>
 
-<button>
+
+
+<button onclick="downloadSong(${index})">
 
 Download
 
 </button>
 
-<button>
+
+
+<button onclick="shareSong(${index})">
 
 Share
 
 </button>
 
+
+
 </div>
+
+
+
 
 <div class="equalizer">
 
 <span></span>
+
 <span></span>
+
 <span></span>
+
 <span></span>
+
 <span></span>
 
 </div>
 
+
+
 </div>
+
+
 
 `;
 
+
 });
 
+
 }
+
+
+
+
+function playSong(index){
+
+
+currentSong = index;
+
+
+audio.src = songs[index].file;
+
+
+audio.play();
+
+
+
+playerTitle.innerText = songs[index].title;
+
+playerArtist.innerText = songs[index].artist;
+
+playerCover.src = songs[index].cover;
+
+
+
+}
+
+
+
+
+function downloadSong(index){
+
+
+const link = document.createElement("a");
+
+
+link.href = songs[index].file;
+
+
+link.download = songs[index].title;
+
+
+link.click();
+
+
+}
+
+
+
+
+function shareSong(index){
+
+
+if(navigator.share){
+
+
+navigator.share({
+
+title:songs[index].title,
+
+url:window.location.href
+
+});
+
+
+}else{
+
+
+alert("لینک صفحه را کپی کنید");
+
+
+}
+
+
+}
+
+
+
+
+audio.addEventListener("ended",()=>{
+
+
+currentSong++;
+
+
+if(currentSong >= songs.length){
+
+currentSong = 0;
+
+}
+
+
+playSong(currentSong);
+
+
+});
+
+
+
+
+showSongs();
